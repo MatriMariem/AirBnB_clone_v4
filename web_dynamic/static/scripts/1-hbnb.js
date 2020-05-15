2612bdc6ec1c0a4e12c8e593e611fe4f8bb3bdca
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-05-15 22:11:16 +0100

    show list of checked amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-const checked_amenities = {};
-$('INPUT[type="checkbox"]').click(function () {
-  if ($(this).prop('checked') === true) {
-    checked_amenities[$(this).data-name] = $(this).data-id;
-  } else {
-    delete checked_amenities[$(this).data-name];
-  }
-  const s = '';
-  for (const key in checked_amenities) {
-    if (s !== '') s += ', ';
-    s += checked_amenities[key];
-  }
-  $('h4').text(s);
-});
+  const ca = {};
+  const name = [];
+  $('INPUT[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
+      ca[$(this).attr('data-name')] = $(this).attr('data-id');
+      name.push($(this).attr('data-name'));
+    } else {
+      delete ca[$(this).attr('data-name')];
+      name.splice(name.indexOf($(this).attr('data-name')), 1);
+    }
+    $('DIV.amenities h4').text(name.join(', '));
+  });
 });
