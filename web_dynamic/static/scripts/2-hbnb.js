8720c40ff1dcd56d024e55b3e193aa3276bfd9aa
Emna <bhmemna7@gmail.com>
2020-05-16 14:41:05 +0000

    fix

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
   });
 const url = 'http://0.0.0.0:5001/api/v1/status/';
 $.getJSON(url, function (data) {
-    if (data['status'] === 'OK') {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
         $('DIV#api_status').removeClass('available');
